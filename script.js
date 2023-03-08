2a67322461219871d451839bb9e36ebd40e92b7e
alex bocknek <alx.bock@gmail.com>
2023-03-08 09:22:49 -0800

    Added divs through DOM manipulation to make 16 x 16, then styled as a grid using CSS grid.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i=1; i<=16; i++) {
+for (let i=1; i<=256; i++) {
     const gridItem = document.createElement('div');
-    gridItem.setAttribute('id', `grid-item${i}`);
+    gridItem.setAttribute('id', `grid-item-${i}`);
     gridContainer.appendChild(gridItem);
+    gridItem.style.backgroundColor = 'white';
+    gridItem.textContent = 'hello';
 
 }
 
