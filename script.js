ae9dce3d750feab00fb0822464e07bf948fdd0c7
alex bocknek <alx.bock@gmail.com>
2023-03-07 16:35:29 -0800

    Finished for loop that creates divs in the grid container div and appends ids to them.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const gridContainer = document.querySelector('#grid-container');
+
 for (let i=1; i<=16; i++) {
-    const div = document.createElement(`div${i}`)
+    const gridItem = document.createElement('div');
+    gridItem.setAttribute('id', `grid-item${i}`);
+    gridContainer.appendChild(gridItem);
+
 }
+
