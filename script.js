5339e21471c4a2d2d5dcbcef57288a3bc9e22fa5
alex bocknek <alx.bock@gmail.com>
2023-03-08 11:16:30 -0800

    Moved the div background coloration from the .js to .css file for cleaner code.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ for (let i=1; i<=256; i++) {
     const gridItem = document.createElement('div');
     gridItem.setAttribute('class', 'grid-item');
     gridContainer.appendChild(gridItem);
-    gridItem.style.backgroundColor = 'white';
 }
 
 const gridItems = document.querySelectorAll('.grid-item');
