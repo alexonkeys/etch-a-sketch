3249a1212d7e7beadf89b5a2e16c8c4ad368c404
alex bocknek <alx.bock@gmail.com>
2023-03-08 10:24:00 -0800

    Changed the IDs of the grid items to classes instead. Created a nodelist for each gridItem and turns the div black if the pointer mouses over the grid.

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const gridContainer = document.querySelector('#grid-container');
 
 for (let i=1; i<=256; i++) {
     const gridItem = document.createElement('div');
-    gridItem.setAttribute('id', `grid-item-${i}`);
+    gridItem.setAttribute('class', 'grid-item');
     gridContainer.appendChild(gridItem);
     gridItem.style.backgroundColor = 'white';
 }
 
+const gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.style.backgroundColor = 'black';
+    });
+});
