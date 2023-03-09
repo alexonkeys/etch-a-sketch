83d72e11dd360d15f7467b146ff01c8a5344df69
alex bocknek <alx.bock@gmail.com>
2023-03-09 14:33:57 -0800

    Reconfigured the slider so that each time the slider value is changed it returns that value to the variable gridSize.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gridContainer = document.querySelector('#grid-container');
+const slider= document.querySelector('#grid-range-slider');
+const sliderValue = document.querySelector('.slider-value');
+const gridSize = slider.value ** 2;
+
 
 for (let i=1; i<=256; i++) {
     const gridItem = document.createElement('div');
@@ -13,3 +17,9 @@ gridItems.forEach((gridItem) => {
       gridItem.style.backgroundColor = 'black';
     });
 });
+
+slider.addEventListener('input', () => {
+    sliderValue.textContent = slider.value;
+    const gridSize = slider.value ** 2;
+    console.log(gridSize);
+  });
